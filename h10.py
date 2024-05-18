a6852d4efd3e909141595f03a6ff36e6b8a8fb95
Adam Griffith <128840129+adamgriffith29@users.noreply.github.com>
2024-05-17 21:13:24 -0500

    Add files via upload

---
+++ h10.py
@@ -0,0 +1,17 @@
+x = 1
+y = 2.0
+z = '3'
+
+x = str(x)
+y = str(y)
+z = str(z)
+
+print('x is ' + x)
+print('y is ' + y)
+print('z is ' + z)
+
+name = "Adam Griffith"
+funky_name = name[::2]
+weird_name = name[1::3]
+print(funky_name)
+print(weird_name)
